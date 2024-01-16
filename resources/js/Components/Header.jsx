37c8d014a6d854d3158a3484aa2f1a5dd8f1f79b
Caique De Brito <caiquedebritoo@gmail.com>
2024-01-15 21:22:03 -0300

    modificação do header

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,15 +1,33 @@
 import NavLink from "./NavLink";
+import TextInput from "./TextInput";
+import Account from "../assets/account.svg";
+import ShoppingCart from "../assets/shopping-cart.svg";
+import ApplicationLogo from "./ApplicationLogo";
 
 export default function Header() {
   return (
-    <header className="w-full bg-blue-600 flex h-24 justify-between items-center px-12">
-      <span className="text-2xl text-gray-50">LancheExpresso</span>
+    <header className="w-full bg-blue-600 flex flex-col pt-6  px-12">
+      <div className="w-full flex justify-between">
+        <ApplicationLogo />
+        <TextInput placeholder="Digite o que você procura..." type="search"/>
 
-      <div className="flex gap-3">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/login">Login</NavLink>
-        <NavLink href="register">Cadastro</NavLink>
+        <div className="flex items-center gap-5">
+          <NavLink href="/login">
+            {/* <img src={Account} alt="Conta" /> */}
+            Login
+          </NavLink>
+          <NavLink>
+            <img src={ShoppingCart} alt="Carrinho de compras" />
+          </NavLink>
+        </div>
       </div>
+
+      <nav className="w-full flex justify-center gap-5 py-3 ">
+        <NavLink href="/">Início</NavLink>
+        <NavLink href="/categorias">Categorias</NavLink>
+        <NavLink href="/sobre-nos">Sobre nós</NavLink>
+        <NavLink href="/contato">Contato</NavLink>
+      </nav>
     </header>
   )
 }
