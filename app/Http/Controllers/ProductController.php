d3d3b918e70e3ce116da1a29a39b206a22c31eb1
Caique De Brito <caiquedebritoo@gmail.com>
2024-01-20 11:18:33 -0300

    criar os métodos de atualizar e apagar produto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
     public function index() {
         $products = Product::all();
         return response()->json($products);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request) {
         $request->validate([
             'name' => 'required|string|max:255',
@@ -29,5 +35,38 @@ class ProductController extends Controller
         ]);
 
         redirect('/admin');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product): RedirectResponse
+    {
+        //
+        // $this->authorize('update', $product);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'description' => 'required|string|max:255',
+            'category' => 'required|string|max:255',
+        ]);
+
+        $product->update($validated);
+
+        return redirect(route('admin.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        //
+        // $this->authorize('delete', $product);
+
+        $product->delete();
+
+        return redirect(route('admin.index'));
     }
 }
