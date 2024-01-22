e11962041fa2091cad6e878064feac0fdae1beac
Caique De Brito <caiquedebritoo@gmail.com>
2024-01-22 16:55:43 -0300

    criar relacionamento n:m entre Category e Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -13,6 +14,10 @@ class Product extends Model
         'name',
         'price',
         'description',
-        'category',
     ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'product_category', 'product_id', 'category_id');
+    }
 }
