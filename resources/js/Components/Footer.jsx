26cd2c5cce710cc461adf3be7cb74095899c2445
Caique <caiquedebritoo@gmail.com>
2024-02-04 21:52:52 -0300

    estilizar footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -12,24 +12,24 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-5 text-sm flex-wrap gap-5">
         <ul>
-          <li>Institucional</li>
-          <li><a href="">Sobre o lanche expresso</a></li>
-          <li><a href="">Política de privacidade</a></li>
-          <li><a href="">Termos de uso</a></li>
+          <li className='font-bold text-base mb-2'>Institucional</li>
+          <li><a href="" className='hover:underline'>Sobre o lanche expresso</a></li>
+          <li><a href="" className='hover:underline'>Política de privacidade</a></li>
+          <li><a href="" className='hover:underline'>Termos de uso</a></li>
         </ul>
         <ul>
-          <li>Atendimento</li>
-          <li><a href="">Sobre o lanche expresso</a></li>
-          <li><a href="">Política de privacidade</a></li>
-          <li><a href="">Termos de uso</a></li>
+          <li className='font-bold text-base mb-2'>Atendimento</li>
+          <li><a href="" className='hover:underline'>Sobre o lanche expresso</a></li>
+          <li><a href="" className='hover:underline'>Política de privacidade</a></li>
+          <li><a href="" className='hover:underline'>Termos de uso</a></li>
         </ul>
         <ul>
-          <li>Institucional</li>
-          <li><a href="">Sobre o lanche expresso</a></li>
-          <li><a href="">Política de privacidade</a></li>
-          <li><a href="">Termos de uso</a></li>
+          <li className='font-bold text-base mb-2'>Institucional</li>
+          <li><a href="" className='hover:underline'>Sobre o lanche expresso</a></li>
+          <li><a href="" className='hover:underline'>Política de privacidade</a></li>
+          <li><a href="" className='hover:underline'>Termos de uso</a></li>
         </ul>
       </div>
     </footer>
