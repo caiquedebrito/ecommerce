fa06697a048c3007699fc485ae7920774fb6832d
Caique De Brito <caiquedebritoo@gmail.com>
2024-02-10 10:35:16 -0300

    criar páginas de contato e sobre

---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,18 @@
+import Footer from '@/Components/Footer'
+import Header from '@/Components/Header'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Sobre nós</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laboriosam error animi dolorum aliquid earum sapiente, facilis libero exercitationem vitae cum. Dolore laborum quibusdam magni. Sint vitae similique cum consequatur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laboriosam error animi dolorum aliquid earum sapiente, facilis libero exercitationem vitae cum. Dolore laborum quibusdam magni. Sint vitae similique cum consequatur.</p>
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
---
+++ resources/js/Pages/Contact.jsx
@@ -0,0 +1,18 @@
+import Footer from '@/Components/Footer'
+import Header from '@/Components/Header'
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Contatos</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laboriosam error animi dolorum aliquid earum sapiente, facilis libero exercitationem vitae cum. Dolore laborum quibusdam magni. Sint vitae similique cum consequatur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laboriosam error animi dolorum aliquid earum sapiente, facilis libero exercitationem vitae cum. Dolore laborum quibusdam magni. Sint vitae similique cum consequatur.</p>
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
