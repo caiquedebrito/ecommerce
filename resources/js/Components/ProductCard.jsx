13f40c42590776b2d5b698abf5e4f4c890f1386b
caique <caiquedebritoo@gmail.com>
2024-09-04 14:57:39 -0300

    fix: productCard Button

--- resources/js/Components/ProductCard.jsx
+++ resources/js/Components/ProductCard.jsx
@@ -4,15 +4,18 @@ import PrimaryButton from "./PrimaryButton";
 export default function ProductCard({ product, admPage = false }) {
 
     return (
-        <a href={admPage ? route('admin.product', product.id) : route('products.show', product.id)}>
-            <div className="h-96 w-64 flex flex-col justify-between items-center border rounded-md p-1">
-                <img src={product.thumbnail} alt={`Imagem do produto ${product.name}`} className="h-64 w-72" />
-                <h3 className="text-xl">{product.name}</h3>
-                <span className="text-xl font-bold">R$ {product.price}</span>
-                <PrimaryButton className="block w-full text-center">
-                    Comprar
-                </PrimaryButton>
-            </div>
-        </a>
+        <div className="h-96 w-64 flex flex-col justify-between items-center border rounded-md p-1">
+            <a href={admPage ? route('admin.product', product.id) : route('products.show', product.id)} className="contents">
+                <div className="flex flex-col justify-between items-center">
+                    <img src={product.thumbnail} alt={`Imagem do produto ${product.name}`} className="h-64 w-72" />
+                    <h3 className="text-xl">{product.name}</h3>
+                    <span className="text-xl font-bold">R$ {product.price}</span>
+                    
+                </div>
+            </a>
+            <PrimaryButton className="block w-full text-center">
+                Comprar
+            </PrimaryButton>
+        </div>
     );
 }
